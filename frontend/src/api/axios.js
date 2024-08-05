4d98e43d96d3e330cff75ba1e705e7157aa5ac27
TrinidadFrancisco01 <atrinifco2004@gmail.com>
2024-08-05 14:25:35 -0600

    modificaciones

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL:"http://localhost:3000",
-    baseURL : "https://procesos-alimentarios-eosin.vercel.app",
+    baseURL : "https://pro-alimentarios-back.vercel.app",
     withCredentials: true,
-})
+});
 
 export default instance; 
